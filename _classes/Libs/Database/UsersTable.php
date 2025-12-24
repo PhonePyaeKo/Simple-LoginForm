2717f939a3ec0e2b53cd43db5401746d3b3849de
PhonePyaeKo <phonepyaeko01@gmail.com>
2025-12-24 16:51:07 +0630

    add/upload.php_uploadPhotoFunction

--- _classes/Libs/Database/UsersTable.php
+++ _classes/Libs/Database/UsersTable.php
@@ -49,4 +49,20 @@ class UsersTable
             exit();
         }
     }
+
+    // Update Photo
+    public function updatePhoto($id, $photo)
+    {
+        try {
+
+            $statement = $this->db->prepare("UPDATE users SET photo=:photo WHERE id=:id");
+            $statement->execute(['id' => $id, 'photo' => $photo]);
+
+            return $statement->rowCount();
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit();
+        }
+    }
 }
